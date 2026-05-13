6121724f0250bd614711fd65390d524d8b4de018
Annalinneee <20221154@nbsc.edu.ph>
2026-05-13 08:08:28 +0800

    added

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     legacy()
   ],
-  base:"it35-lab/",
+
+  base: "/AiPracticeBot/",
+
   test: {
     globals: true,
     environment: 'jsdom',
